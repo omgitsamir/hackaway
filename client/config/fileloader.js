bb632fd99864fb496ae479cba0251d79a6054e24
parsaEbi <parsa.ebrahimi@kcl.ac.uk>
2020-02-02 04:52:15 +0000

    some styling

--- client/config/fileloader.js
+++ client/config/fileloader.js
@@ -4,7 +4,11 @@ const fileLoader = game => {
   game.load.crossOrigin = 'Anonymous'
   game.stage.backgroundColor = '#1E1E1E'
   game.load.image('stadium', `${ASSETS_URL}/sprites/stadium/stadium.png`)
-  game.load.image('man', `${ASSETS_URL}/sprites/man/man.png`)
+  game.load.image('man1', `${ASSETS_URL}/sprites/man/man1.png`)
+  game.load.image('man2', `${ASSETS_URL}/sprites/man/man2.png`)
+  game.load.image('man3', `${ASSETS_URL}/sprites/man/man3.png`)
+  game.load.image('man4', `${ASSETS_URL}/sprites/man/man4.png`)
+
 }
 
 export default fileLoader
